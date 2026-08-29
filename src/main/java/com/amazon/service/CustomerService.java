d52b9a0decbbdca244860ef72a5978da7e4ee0d3
anamikakumari-bca <anamikakumaribca1@gmail.com>
2026-08-29 13:29:41 +0530

    Copy CustomerService from AmazonApplication

---
+++ src/main/java/com/amazon/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.amazon.service;
+
+import com.amazon.dao.CustomerDAO;
+
+public class CustomerService {
+    private CustomerDAO customerDAO;
+    public CustomerService() { customerDAO = new CustomerDAO(); }
+    public boolean login(String email, String password) { return customerDAO.login(email, password); }
+    public int getCustomerId(String email, String password) { return customerDAO.getCustomerId(email, password); }
+}
